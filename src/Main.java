2cb8d6752c83ac485bcbdf05d26005b932f3650b
baktulan <bak2landeveloper@gmail.com>
2024-05-04 15:00:54 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+     Triangle triangle= new Triangle();
+     triangle.getArea(triangle.a=4, triangle.b=5, triangle.c=3);
+
+    }
+}
